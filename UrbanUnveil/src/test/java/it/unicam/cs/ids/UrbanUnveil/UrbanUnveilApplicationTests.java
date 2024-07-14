7e268389bc3e049840befee195b5c002578bf0da
matluk24 <137879264+matluk24@users.noreply.github.com>
2024-07-15 01:47:17 +0200

    Prova per sistemare

---
+++ UrbanUnveil/src/test/java/it/unicam/cs/ids/UrbanUnveil/UrbanUnveilApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.UrbanUnveil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanUnveilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

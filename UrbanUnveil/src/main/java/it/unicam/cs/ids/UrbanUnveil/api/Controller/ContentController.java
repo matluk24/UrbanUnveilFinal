4fbd19d71d14aeab61c5c221e2eb6725c0e5dfa2
francescobarontini <atomf@DESKTOP-HKN2KAU>
2024-08-20 13:13:12 +0200

    Content Controller update, Upload

--- UrbanUnveil/src/main/java/it/unicam/cs/ids/UrbanUnveil/api/Controller/ContentController.java
+++ UrbanUnveil/src/main/java/it/unicam/cs/ids/UrbanUnveil/api/Controller/ContentController.java
@@ -1,5 +1,6 @@
 package it.unicam.cs.ids.UrbanUnveil.api.Controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.unicam.cs.ids.UrbanUnveil.api.DesignPattern.ContentServiceFactory;
@@ -26,6 +28,7 @@ public class ContentController {
 
     @Autowired
     private ContentServiceFactory contentServiceFactory;
+	private static final String UPLOAD_DIR = "./Utils/Content/";
     
     public ContentController(ContentServiceFactory o) {
     	contentServiceFactory=o;
@@ -35,21 +38,40 @@ public class ContentController {
 		
 	}
     
-    @PostMapping("/save")
-    public ResponseEntity<Content> saveContent(@RequestBody Content content) throws IOException {
-        ContentService contentService = contentServiceFactory.getService(content);
-        
-        Content c = contentService.save(content);
-        HttpStatus httpStatus = HttpStatus.CONFLICT;
-        
-        if(c.equals(content)) {
-        	httpStatus = HttpStatus.OK;
-        }else if (c == null)
-			httpStatus = HttpStatus.BAD_REQUEST;
-        
-        return new ResponseEntity<Content>(c, httpStatus);
-    }
 
+
+    @PostMapping("/uploadContent")
+    public ResponseEntity<Content> handleContentUpload(@RequestBody Content content, MultipartFile file) throws IOException{
+        if (file.isEmpty()) {
+            return new ResponseEntity<Content>(content, HttpStatus.BAD_REQUEST);
+        }
+
+            String contentType = file.getContentType();
+            String fileName = file.getOriginalFilename();
+            File dest = null;
+
+
+            // Gestione specifica basata sul tipo di file
+            if (contentType != null) {
+                if (contentType.startsWith("image/")) {
+                	dest = new File(UPLOAD_DIR +"image/"+ fileName);
+                } else if (contentType.startsWith("video/")) {
+                	dest = new File(UPLOAD_DIR +"video/"+ fileName);
+                } else if (contentType.startsWith("application/pdf")) {
+                	dest = new File(UPLOAD_DIR +"file/"+ fileName);
+                }
+            }
+            ContentService contentService = contentServiceFactory.getService(content);
+            file.transferTo(dest);
+            
+            Content c = contentService.save(content);
+            HttpStatus httpStatus = HttpStatus.CONFLICT;
+            if(c.equals(content)) {
+            	httpStatus = HttpStatus.OK;
+            }
+            return new ResponseEntity<Content>(c, httpStatus);
+        }
+       
     @GetMapping("/load")
     public ResponseEntity<Content> loadContent(@RequestBody Long id, ContentEnum contentEnum) throws IOException {
         // Logica per determinare il tipo di contenuto basato su contentEnum

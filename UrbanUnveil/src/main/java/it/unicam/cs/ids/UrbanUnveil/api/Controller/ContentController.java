f2b50f95b7f1e0d8318cc01b77035a60ac2d22ee
franc <franc@192.168.56.1>
2024-09-22 11:04:41 +0200

    patch: conflict path

--- UrbanUnveil/src/main/java/it/unicam/cs/ids/UrbanUnveil/api/Controller/ContentController.java
+++ UrbanUnveil/src/main/java/it/unicam/cs/ids/UrbanUnveil/api/Controller/ContentController.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,7 +20,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 
 import it.unicam.cs.ids.UrbanUnveil.api.DesignPattern.ContentHandlerFactory;
 import it.unicam.cs.ids.UrbanUnveil.api.DesignPattern.ContentServiceFactory;
